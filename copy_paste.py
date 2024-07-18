79ab4a1b331a7eca9e356b138622881dc2fcf952
Mariano Barberi Garza <88672243+MarianoBarberi@users.noreply.github.com>
2024-07-18 05:38:54 -0300

    added my lists, works with every language now

--- copy_paste.py
+++ copy_paste.py
@@ -29,15 +29,13 @@ if st.button('Calcular'):
         follower_after_split = follower_list.split('\n')
         follower_final_list = []
         for followers in follower_after_split:
-            if 'profile picture' in followers:
-                followers = followers.replace("'s profile picture", '')
+            if ' ' not in followers and not any(c.isupper() for c in followers):
                 follower_final_list.append(followers)
 
         following_after_split = following_list.split('\n')
         following_final_list = []
         for following in following_after_split:
-            if 'profile picture' in following:
-                following = following.replace("'s profile picture", '')
+            if ' ' not in following and not any(c.isupper() for c in following):
                 following_final_list.append(following)
         not_followed_back = [user for user in following_final_list if user not in follower_final_list]
         st.title("Usuarios que no te siguen:")
