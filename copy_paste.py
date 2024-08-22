228f46c7c6b7dd25f02782326ff4544c050259ed
Mariano Barberi Garza <88672243+MarianoBarberi@users.noreply.github.com>
2024-08-22 16:54:11 -0300

    use identity instead of for loop

--- copy_paste.py
+++ copy_paste.py
@@ -19,27 +19,28 @@ with st.container():
             st.video('https://youtu.be/HniVZmzjoBU')
 
 st.write('Copia y pega la lista de seguidores y seguidos de Instagram.')
-follower_list = st.text_area('Lista de seguidores:')
-following_list = st.text_area('Lista de seguidos:')
 
-follower_final_list = set()
-following_final_list = set()
+followers_list = st.text_area('Lista de seguidores:')
+followings_list = st.text_area('Lista de seguidos:')
+
 
 if st.button('Calcular'):
-    if follower_list == '' or following_list == '':
+    if followers_list == '' or followings_list == '':
         st.warning('Por favor, llena los campos.')
     else:
-        follower_after_split = follower_list.split('\n')
-        for followers in follower_after_split:
-            if ' ' not in followers and not any(c.isupper() for c in followers):
-                follower_final_list.add(followers)
+        # followers_after_split = followers_list.split('\n')
+        # for follower in followers_after_split:
+        #     if ' ' not in follower and not any(character.isupper() for character in follower):
+        #         followers_final_list.add(follower)
+        followers_final_list = {follower.strip() for follower in followers_list.split('\n') if ' ' not in follower and follower.islower()}
 
-        following_after_split = following_list.split('\n')
-        for following in following_after_split:
-            if ' ' not in following and not any(c.isupper() for c in following):
-                following_final_list.add(following)
+        # followings_after_split = followings_list.split('\n')
+        # for following in followings_after_split:
+        #     if ' ' not in following and not any(character.isupper() for character in following):
+        #         followings_final_list.add(following)
+        followings_final_list = {following.strip() for following in followings_list.split('\n') if ' ' not in following and following.islower()}
 
-        not_followed_back = following_final_list - follower_final_list
+        not_followed_back_list = followings_final_list - followers_final_list
         st.title("Usuarios que no te siguen:")
-        for user in not_followed_back:
+        for user in not_followed_back_list:
             st.write(user)
